1f30010eca81fd454ce58190143a0d15475bd9ae
HalCharymyradov <hcharymyradov@gmail.com>
2025-05-04 18:12:00 -0400

    Use Apex to Work with Data module changes

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,33 @@
+import { LightningElement, api, track, wire} from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+export default class ContactList extends LightningElement {
+    contacts;
+    @track error;
+    columns = [
+        { label: 'First Name', fieldName: 'FirstName', type: 'text' },
+        { label: 'Last Name', fieldName: 'LastName', type: 'text' },
+        { label: 'Email', fieldName: 'Email', type: 'email' },
+    ];
+
+    @wire(getContacts)
+    contacts({ error, data }) {
+        console.log('halTrial: contacts CALLED: ' + JSON.stringify(data));
+        if (data) {
+            this.contacts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error loading contacts',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            );
+        }
+    }
+
+}
